2cdca5390bf65f2dcadbb6e4592ff32272bb6e75
github-irshad <irshadaj08@gmail.com>
2025-01-22 21:31:51 +0530

    explore menu completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your favorite food here!</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eum
+          magnam, cupiditate velit animi dignissimos veniam odio, earum
+          voluptates voluptatem omnis nulla ea alias iure blanditiis optio
+          numquam temporibus sed.
+        </p>
+        <button>View More</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
