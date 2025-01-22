2605ae006e9b708c21bb6aad9acb665e9a34375a
github-irshad <irshadaj08@gmail.com>
2025-01-22 16:10:57 +0530

    Setting up Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { assets } from './assets/assets'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { assets } from "./assets/assets";
+import { Routes, Route } from "react-router-dom";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
