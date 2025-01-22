2622a944f3e8eee623d4cf33310ef6a5eecd6b6e
github-irshad <irshadaj08@gmail.com>
2025-01-22 23:37:13 +0530

    Food item listing

---
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
+
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you!</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              description={item.description}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />)
+          ;
+        })}
+
+      </div>
+    </div>
+  );
+};
+
+export default FoodDisplay;
