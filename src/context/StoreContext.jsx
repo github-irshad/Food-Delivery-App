b0a64f08c57db1246f5bc4334746dcf47e0bbb5d
github-irshad <irshadaj08@gmail.com>
2025-01-24 01:06:30 +0530

    Front End Completed

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
@@ -19,8 +19,23 @@ const StoreContextProvider = (props) => {
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
+
+  const getTotalCartAmount = ()=>{
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+
+        let itemInfo = food_list.find((product)=>product._id===item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
+  
+
   const contextValue = {
     food_list,
+    getTotalCartAmount,
     cartItems,
     setCartItems,
     removeFromCart,
