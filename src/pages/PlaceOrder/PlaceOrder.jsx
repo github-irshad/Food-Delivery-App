b9f188098713e75aafecdc2d2c262d9c5bdaf726
github-irshad <irshadaj08@gmail.com>
2025-01-24 20:48:42 +0530

    github page unpublished

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
-import './PlaceOrder.css'
+import "./PlaceOrder.css";
 const PlaceOrder = () => {
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
-    <form className="place-order">
+    <form action="" className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
@@ -24,25 +24,27 @@ const PlaceOrder = () => {
         <input type="text" placeholder="Phone" />
       </div>
       <div className="place-order-right">
-      <div className="cart-total">
+        <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()??0}</p>
+              <p>${getTotalCartAmount() ?? 0}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
-          <button>PROCEED TO PAYMENT</button >
+          <button>PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
